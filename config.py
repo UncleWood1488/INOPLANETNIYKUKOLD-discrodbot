a5c4c5dcd2b5ed68e3831fddc8d4aaba045f2497
UncleWood <55793840+UncleWood1488@users.noreply.github.com>
2025-12-22 01:29:23 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
 import os
 from random import *
-BOT_TOKEN = os.environ.get('BOT_TOKEN') or "NjAzOTM4OTUzNzQ5NDYzMDUw.GGMGPi.jqEIV5-X2Wh3jm99wUaNLn6Zj6qEj6nufk0Pm8"
+BOT_TOKEN = os.environ.get('BOT_TOKEN') or ""
 BOT_PREFIX = os.environ.get('BOT_PREFIX') or '.'
 # PROXY_SCHEMA = os.environ.get('PROXY_SCHEMA') or "http"
 # PROXY_HOST = os.environ.get('PROXY_HOST') or "localhost"
 # PROXY_PORT = os.environ.get('PROXY_PORT') or 3130
 # PROXY_USER = os.environ.get('PROXY_USER') or ""
 # PROXY_PASSWORD = os.environ.get('PROXY_PASSWORD') or ""
+
+# Роли модераторов
+MOD_ROLE_IDS = [406211889228546048, 406212152316395574]
+
+FFMPEG_PATH = "C:/Users/UncleWood/Desktop/Programming/INOPLANETNIYKUKOLD-discrodbot-main/FFmpeg/bin/ffmpeg.exe"
+
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -nostdin',
+    'options': '-vn -sn -dn -ignore_unknown -hide_banner -loglevel error'
+}
+
 
 # Настройки кулдаунов (в секундах)
 cooldown = {
@@ -47,5 +58,21 @@ fish_data = {
     'salmon': {'name': 'Лосось', 'emoji': '<:Fish_Raw_Salmon:1327154686335385641>'},
     'tropical': {'name': 'Тропическая рыба', 'emoji': '<:Fish_Tropical:1327154699383607317>'},
     'squid': {'name': 'Кальмар', 'emoji': '<:Fish_Squid:1327427600888500326>'}
+}
+
+# Настройки карты
+MAP_SETTINGS = {
+    'grid_size': 50,
+    'cell_size': 20,
+    'view_radius': 5
+}
+
+# svo
+weapons = {
+
+}
+
+vehicle = {
+    
 }
 #game war-------
